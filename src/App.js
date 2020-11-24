378499b54f536e37d62a163a3ac76c7e7ef1cc1a
mike1934 <74881270+mike1934@users.noreply.github.com>
2020-11-24 14:11:28 +1300

    2 commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React ,{Component} from 'react'
+import {BrowserRouter,Switch,Route}from 'react-router-dom'
+
+import Login from './pages/login/login'
+import Admin from './pages/admin/admin'
+
+
+
+
+class App extends Component {
+
+   
+    render () {
+        return (
+            <BrowserRouter>
+               <Switch>
+                   <Route path='/login' component={Login}/>
+                   <Route               component={Admin}/>
+               </Switch>
+            </BrowserRouter>     
+        )
+    }
+}
+
+export default App
